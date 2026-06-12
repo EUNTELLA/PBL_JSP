3de49537a6b88e20f01e1aa2231f13abe625ebbe
EUNTELLA <yokohamay0612@gmail.com>
2026-06-12 16:26:13 +0900

    feat: root 프로필 이미지 추가 및 관련 SQL 수정

--- src/main/java/util/ProfileUtil.java
+++ src/main/java/util/ProfileUtil.java
@@ -2,9 +2,14 @@ package util;
 
 public class ProfileUtil {
     public static String resolveImage(String uid, String profileImage) {
-        if ("kim@abc.com".equals(uid)) return "profile1.jpg";
-        if ("lee@abc.com".equals(uid)) return "profile2.jpg";
-        if ("kwon@abc.com".equals(uid)) return "profile3.jpg";
+        if ("kim@abc.com".equals(uid))
+            return "profile1.jpg";
+        if ("lee@abc.com".equals(uid))
+            return "profile2.jpg";
+        if ("kwon@abc.com".equals(uid))
+            return "profile3.jpg";
+        if ("root@abc.com".equals(uid))
+            return "rootprofile.jpg";
         return profileImage;
     }
 }
